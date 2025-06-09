b6b07e852517219c05444dce0ede833fd820dc01
Randyglobal <randyitoe2255@gmail.com>
2025-06-09 08:21:19 -0400

    Practicing CRUD on mysql

---
+++ basic-queries/insert-update-delete.sql
@@ -0,0 +1,45 @@
+use northwind;
+-- insert into 
+--           suppliers (CompanyName, 
+--                       ContactName,
+-- 					  ContactTitle,
+--                       Address, 
+--                       City,
+--                       Region, 
+--                       PostalCode, 
+--                       Country, 
+--                       Phone, 
+--                       Fax, 
+--                       HomePage)
+--  values("Randy Corp", "Randy Lol", "CTO", "12345 Quils St", "Charlotte", "Litoral", "237", "Cameroon", "368768798968", "0014", "");
+
+-- insert into 
+--            products (ProductName, 
+-- 					SupplierID, 
+--                     CategoryID, 
+--                     QuantityPerUnit, 
+--                     UnitPrice, 
+--                     UnitsInStock, 
+--                     UnitsOnOrder, 
+--                     ReorderLevel, 
+--                     Discontinued)
+-- values ("Sun Glasses", 30, 2 , "20 pieces", 3.2000, 12, 20, 11, 0);
+
+-- Select * from products
+
+-- update products set UnitPrice = 3.200 * 0.46
+-- where ProductID = 78;
+
+
+-- Select ProductName, UnitPrice from products
+
+-- Delete from products where ProductID = 78;
+
+-- Delete from suppliers where SupplierID = 30;
+
+-- Select * from products;
+
+-- select * from suppliers;
+
+
+ 
